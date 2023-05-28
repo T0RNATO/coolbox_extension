816296287094223c461e5c7e9a2f3913aa6a9fc9
SleepyStew <sleepystew3@gmail.com>
2023-05-28 12:34:03 +1000

    added error handling to pretty subjects

--- extension/js/timetable.js
+++ extension/js/timetable.js
@@ -2,8 +2,17 @@ chrome.storage.local.get(["subjects"]).then((subjects) => {
     const names = subjects.subjects.subjects;
 
     for (const subject of document.querySelectorAll(`[data-timetable] td a`)) {
-        subject.innerText = names.filter(sub => {
-            return sub.name.toLowerCase() === subject.nextElementSibling.firstChild.textContent.replace(/[()]/g, "").toLowerCase();
-        })[0].pretty;
+        try {
+            const prettySubject = names.find(sub => sub.name.toLowerCase() === subject.nextElementSibling.firstChild.textContent.replace(/[()]/g, "").toLowerCase());
+            if (prettySubject !== undefined) {
+                subject.innerText = prettySubject.pretty;
+            } else {
+                console.log(`No pretty subject found for ${subject.innerText}`);
+            }
+        } catch (error) {
+            console.error(error);
+        }
     }
-})
+}).catch((error) => {
+    console.error(error);
+});
